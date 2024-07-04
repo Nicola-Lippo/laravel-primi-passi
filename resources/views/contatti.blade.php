bf7a562e885a34a9be5be385b09c746737a78347
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-04 18:14:45 +0200

    [UPD - FIX] seconda rotta

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel primi passi</title>
+    <title>Contatti</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>Contatti seconda pagina Laravel</h1>
 </body>
 </html>
